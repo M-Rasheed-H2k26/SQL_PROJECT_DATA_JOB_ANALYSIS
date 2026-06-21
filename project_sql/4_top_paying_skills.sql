96f1ac83dd1f17c7c16b4180e4e3358150884787
M-Rasheed-H2k25 <rasheedhussain2k26@gmail.com>
2026-06-21 17:08:16 +0500

    upload sql files

---
+++ project_sql/4_top_paying_skills.sql
@@ -0,0 +1,182 @@
+/* 
+Answer : What are the top skills based on salary?
+- Look at the average salary associated with each skill for 
+  Data Analyst positions.
+- Focuses on roles with specified salaries, regardless of location.
+- Why? It reveals how different skills impact salary levels for Data Analyst and
+  helps identify the most financially rewarding skills to acquire or improve.
+*/
+
+SELECT 
+    skills,
+    AVG (salary_year_avg) AS avg_salary
+FROM 
+    job_postings_fact
+INNER JOIN skills_job_dim 
+ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim 
+ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    -- (to look for remote job) AND job_work_from_home = True
+GROUP BY
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 25
+
+-- to remove the decimal numbers, add ROUND (AVG (), 0)
+
+SELECT 
+    skills,
+    ROUND (AVG (salary_year_avg), 0) AS avg_salary
+FROM 
+    job_postings_fact
+INNER JOIN skills_job_dim 
+ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim 
+ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    -- (to look for remote job) AND job_work_from_home = True
+GROUP BY
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 25
+
+-- now we will check them for remote jobs
+
+SELECT 
+    skills,
+    ROUND (AVG (salary_year_avg), 0) AS avg_salary
+FROM 
+    job_postings_fact
+INNER JOIN skills_job_dim 
+ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim 
+ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = True
+GROUP BY
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 25
+
+/*
+Here's a breakdown of the results for top paying skills for Data Analyst position:
+- Big data skills such as PySpark ($208K), Databricks ($142K), and Airflow ($126K) 
+  are linked to the highest-paying data analyst roles.
+- Programming and ML skills like Pandas ($152K), NumPy ($144K), and DataRobot ($155K)
+  show that advanced analytics expertise commands higher salaries.
+- Cloud and DevOps tools including GCP ($123K), Kubernetes ($133K), and GitLab ($155K) 
+  indicate strong demand for analysts with technical and engineering-focused skills.
+
+[
+  {
+    "skills": "pyspark",
+    "avg_salary": "208172"
+  },
+  {
+    "skills": "bitbucket",
+    "avg_salary": "189155"
+  },
+  {
+    "skills": "couchbase",
+    "avg_salary": "160515"
+  },
+  {
+    "skills": "watson",
+    "avg_salary": "160515"
+  },
+  {
+    "skills": "datarobot",
+    "avg_salary": "155486"
+  },
+  {
+    "skills": "gitlab",
+    "avg_salary": "154500"
+  },
+  {
+    "skills": "swift",
+    "avg_salary": "153750"
+  },
+  {
+    "skills": "jupyter",
+    "avg_salary": "152777"
+  },
+  {
+    "skills": "pandas",
+    "avg_salary": "151821"
+  },
+  {
+    "skills": "elasticsearch",
+    "avg_salary": "145000"
+  },
+  {
+    "skills": "golang",
+    "avg_salary": "145000"
+  },
+  {
+    "skills": "numpy",
+    "avg_salary": "143513"
+  },
+  {
+    "skills": "databricks",
+    "avg_salary": "141907"
+  },
+  {
+    "skills": "linux",
+    "avg_salary": "136508"
+  },
+  {
+    "skills": "kubernetes",
+    "avg_salary": "132500"
+  },
+  {
+    "skills": "atlassian",
+    "avg_salary": "131162"
+  },
+  {
+    "skills": "twilio",
+    "avg_salary": "127000"
+  },
+  {
+    "skills": "airflow",
+    "avg_salary": "126103"
+  },
+  {
+    "skills": "scikit-learn",
+    "avg_salary": "125781"
+  },
+  {
+    "skills": "jenkins",
+    "avg_salary": "125436"
+  },
+  {
+    "skills": "notion",
+    "avg_salary": "125000"
+  },
+  {
+    "skills": "scala",
+    "avg_salary": "124903"
+  },
+  {
+    "skills": "postgresql",
+    "avg_salary": "123879"
+  },
+  {
+    "skills": "gcp",
+    "avg_salary": "122500"
+  },
+  {
+    "skills": "microstrategy",
+    "avg_salary": "121619"
+  }
+]
+*/
---
+++ project_sql/5_optimal_skills.sql
@@ -0,0 +1,133 @@
+/*
+Answer: What are the most optimal skills to learn 
+(aka it's in high demand and a high-paying skill)?
+- Identify skills in high demand and associated with high average salaries 
+  for Data Analyst roles.
+- Concentrates on remote positions with specified salaries.
+- Why? Targets skills that offer job security (high demand) 
+  and financial benefits (high salaries), offering strategic
+  insights for career development in data analysis.
+*/
+
+WITH skills_demand AS ( -- THIS IS FROM THE 3rd FILE OF OUR PROJECT
+    SELECT 
+        skills,
+        COUNT (skills_job_dim.job_id) AS demand_count
+    FROM 
+        job_postings_fact
+    INNER JOIN skills_job_dim 
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim 
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst' AND -- to look for remote job
+        job_work_from_home = True
+    GROUP BY
+        skills
+    ORDER BY
+        demand_count DESC
+    LIMIT 5
+)
+
+WITH average_salary AS ( -- THIS IS FROM THE 4th FILE OF OUR PROJECT
+    SELECT 
+        skills,
+    ROUND (AVG (salary_year_avg), 0) AS avg_salary
+    FROM 
+        job_postings_fact
+    INNER JOIN skills_job_dim 
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim 
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+        AND job_work_from_home = True
+    GROUP BY
+        skills
+    ORDER BY
+        avg_salary DESC
+    LIMIT 25
+)
+
+/* 
+we will add changes to the work from the previous sections. 
+We will be connecting both tables by adding skill_id. 
+We will erase the order by and limit.
+And copy salary not being null from table 4 to table 3.
+Lastly we will combine them adding another select clause at the bottom
+*/
+
+WITH skills_demand AS ( -- THIS IS FROM THE 3rd FILE OF OUR PROJECT
+    SELECT 
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT (skills_job_dim.job_id) AS demand_count
+    FROM 
+        job_postings_fact
+    INNER JOIN skills_job_dim 
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim 
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst' AND -- to look for remote job
+        job_work_from_home = True
+        AND salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_dim.skill_id
+),  average_salary AS ( -- THIS IS FROM THE 4th FILE OF OUR PROJECT (WITH is removed)
+    SELECT
+        skills_job_dim.skill_id,
+        ROUND (AVG (salary_year_avg), 0) AS avg_salary
+    FROM 
+        job_postings_fact
+    INNER JOIN skills_job_dim 
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim 
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short = 'Data Analyst'
+        AND salary_year_avg IS NOT NULL
+        AND job_work_from_home = True
+    GROUP BY
+        skills_job_dim.skill_id
+)
+
+SELECT 
+    skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM 
+    skills_demand
+INNER JOIN average_salary 
+ON skills_demand.skill_id = average_salary.skill_id
+-- now we will use ORDER BY and LIMIT clauses
+ORDER BY
+    demand_count DESC,
+    avg_salary DESC
+LIMIT 25
+
+-- The above work was to demonstrate how to use CTEs. Now we will use a shorter way to get it done.
+
+SELECT
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT (skills_job_dim.job_id) AS demand_count,
+    ROUND (AVG (job_postings_fact.salary_year_avg), 0) AS avg_salary
+FROM 
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = True
+GROUP BY
+    skills_dim.skill_id
+HAVING 
+    COUNT (skills_job_dim.job_id) > 10
+ORDER BY
+    avg_salary DESC,
+    demand_count DESC
+LIMIT 25
